4950d8b668d18ebc4162461a93906b5bd8372fe7
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2023-06-27 09:47:42 +0900

    rename modules

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { decode } from "wasm";
+import { decode } from "friendly_id_wasm";
 
 const input = document.getElementById("input") as HTMLInputElement;
 const dst = document.getElementById("dst") as HTMLInputElement;
